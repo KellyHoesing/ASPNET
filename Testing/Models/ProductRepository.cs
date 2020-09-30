64f6786380cd3506138bd55f68824a725494f8e4
Kelly <kelly.hoesing@gmail.com>
2020-09-30 18:26:04 -0500

    Before Control fiddling version

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+using System.ComponentModel.Design;
+
+namespace Testing.Models
+{
+    public class ProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
+    
